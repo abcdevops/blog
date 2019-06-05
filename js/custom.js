12eccd2e8abd00f53e6ff1e675d395c26283a9a4
tarunjangra <tarun@izap.in>
2019-06-05 15:16:16 +0000

    new version Wed Jun  5 15:16:16 UTC 2019

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,41 @@
 jQuery(document).ready(function($) {
+        //header fix
+        function minheader() {
+            if (window.matchMedia('(min-width: 990px)').matches) {     
+                var wd = $(window).scrollTop();
+        
+                if (wd >= 100) {
+                    $("main  header").addClass("min-header");
+                    $(".min-header .logo").attr("src", "/images/logo-abc-home.png");
+                }
+                else {
+                    $(".min-header .logo").attr("src", "/images/logo-abc-home.png");
+                    $("main header").removeClass("min-header");
+                }
+            }    
+        }
+        minheader();
+        $(document).scroll(function () {
+            minheader();
+        });
+    
+        
+    // share js
+    $(window).scroll(function() {
+        if (window.matchMedia('(min-width: 990px)').matches) {     
+            var scroll = $(window).scrollTop();
+            if (scroll >= 200) {
+                $(".share").addClass("ss_top");
+            }
+            else {
+                $(".share").removeClass("ss_top");
+            }
+        }    
+    }); 
+    
+            
+
+
     $('.mobile-menu').click(function() {
         $('.menu-responsive').toggleClass('m-show');
       });
