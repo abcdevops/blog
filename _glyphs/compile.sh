193556c0598cf29e6a41821854a0f070454825ea
Tarun Jangra <tarun@izap.in>
2019-06-12 18:38:00 -0400

    execute all test

--- _glyphs/compile.sh
+++ _glyphs/compile.sh
@@ -12,5 +12,5 @@ dir=$(dirname "$x")
 target=$1
 
 mkdir -p "${target}"
-bundle exec fontcustom compile "${dir}" "--output=${target}" --font-name=icons --templates=css --no-hash --force --autowidth
+fontcustom compile "${dir}" "--output=${target}" --font-name=icons --templates=css --no-hash --force --autowidth
 sed -i -E "s|\"./icons([^\"]+)\"|\"/css/icons\1?$(date +%s)\"|g" "${target}/icons.css"
